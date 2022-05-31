719bb8d5a9eaf012530af2638aa3a056807553f8
andrewlew1s <andrew12lewis@gmail.com>
2022-05-30 19:05:21 -0700

    feat: v3 shuttle

---
+++ augmented-reality/public/v3/toss-object.js
@@ -0,0 +1,108 @@
+// Copyright (c) 2021 8th Wall, Inc.
+/* globals AFRAME */
+
+const OBJECT_SIZE = 0.1
+const THROW_SPEED = 100
+
+AFRAME.registerComponent('shoot-shuttle', {
+	init() {
+	  const camera = document.getElementById('camera')
+	  const splatSnd = document.querySelector('#splat').components.sound
+
+	  this.el.sceneEl.addEventListener('touchstart', (event) => {
+		console.log(`Throwing object sized ${OBJECT_SIZE} with speed ${THROW_SPEED}`)
+
+		// Create element to be thrown, setting position, scale, and model
+		const shuttle = document.createElement('a-entity')
+		shuttle.setAttribute('position', camera.object3D.position)
+		shuttle.setAttribute('scale', `${OBJECT_SIZE} ${OBJECT_SIZE} ${OBJECT_SIZE}`)
+		shuttle.setAttribute('gltf-model', '#shuttleModel')
+
+		// Choose a random rotation offset for some variation
+		// const randomRotation = {x: -90 + Math.random() * 30, y: Math.random() * 360, z: 0}
+		// shuttle.setAttribute('rotation', randomRotation)
+		shuttle.setAttribute('rotation', '0 0 0')
+
+		// Set velocity, rotated with camera direction
+		const velocity = new THREE.Vector3(0, 0, -THROW_SPEED)
+		velocity.applyQuaternion(camera.object3D.quaternion)
+		shuttle.setAttribute('velocity', velocity)
+
+		// Add physics body
+		shuttle.setAttribute('body', {
+		  type: 'dynamic',
+		  sphereRadius: OBJECT_SIZE / 2,
+		  shape: 'sphere',
+		})
+
+		shuttle.setAttribute('shadow', {
+		  receive: false,
+		})
+
+		// Add shuttle to scene
+		this.el.sceneEl.appendChild(shuttle)
+
+		// The splat is created at the same time as the thrown shuttle so
+		// there is time to load the model before it hits the ground
+		const splatBase = document.createElement('a-entity')
+		splatBase.setAttribute('visible', 'false')
+
+		// The splat consists of a model wrapped in an empty
+		// parent so we can apply the correct scaling animation
+		const splat = document.createElement('a-entity')
+		splat.setAttribute('gltf-model', '#shuttleModel')
+		splat.setAttribute('scale', '1 1 1')
+		splatBase.appendChild(splat)
+
+		this.el.sceneEl.appendChild(splatBase)
+
+		let didCollide = false
+		shuttle.addEventListener('collide', (e) => {
+		  // Only want to do the splat once, and with the ground only
+		  if (didCollide || e.detail.body.el.id !== 'ground') {
+			return
+		  }
+		  didCollide = true
+
+		  // Stop previous splat sound
+		  splatSnd.stopSound()
+		  // Play splat sound
+		  splatSnd.playSound()
+
+		  // Copy positioning of thrown shuttle to splat
+		  splatBase.object3D.position.copy(shuttle.object3D.position)
+		  splat.object3D.rotation.copy(shuttle.object3D.rotation)
+
+		  splatBase.object3D.visible = true
+
+		  shuttle.setAttribute('visible', 'false')
+
+		  // We can't remove the thrown shuttle until the physics step is over
+		  setTimeout(() => {
+			shuttle.parentNode.removeChild(shuttle)
+		  }, 0)
+
+		  // Using animation component to show flattening
+		  splatBase.setAttribute('animation__scale', {
+			property: 'scale',
+			from: '1 1 1',
+			to: '3 0.1 3',
+			dur: 500,
+			easing: 'easeOutQuad',
+		  })
+
+		  // After 2.5 seconds, shrink the splat away and delete it
+		  setTimeout(() => {
+			splatBase.setAttribute('animation__scale', {
+			  property: 'scale',
+			  from: '3 0.1 3',
+			  to: '0.001 0.001 0.001',
+			  dur: 1500,
+			  easing: 'easeInQuad',
+			})
+			setTimeout(() => splatBase.parentNode.removeChild(splatBase), 1500)
+		  }, 2500)
+		})
+	  })
+	},
+  })
